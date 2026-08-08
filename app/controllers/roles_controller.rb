427ed6b633a37b9837379d7f037bc569f9f686a0
yogendra singh <yogend15@gmail.com>
2026-08-08 17:08:20 +0000

    Role model crud completed

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,9 +1,15 @@
 class RolesController < InheritedResources::Base
+  actions :index, :show, :new, :create, :edit, :update, :destroy
+
+  protected
+
+  def collection
+    @roles = Role.ordered
+  end
 
   private
 
-    def role_params
-      params.require(:role).permit()
-    end
-
+  def role_params
+    params.require(:role).permit(:name, :user_id)
+  end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,5 @@
 class Role < ApplicationRecord
-	
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  scope :ordered, -> { order(:name) }
 end
